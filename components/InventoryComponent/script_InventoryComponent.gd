6c670515a0fcfca53c67ab6bb3d8eefce61e1636
andrew <andrew@corsini.dev>
2024-12-14 22:43:50 -0600

    modified inventory component, added ability to delete inventory

--- components/InventoryComponent/script_InventoryComponent.gd
+++ components/InventoryComponent/script_InventoryComponent.gd
@@ -4,15 +4,13 @@ extends Window
 var id: int
 
 @onready var addItemButton: Button = $PanelContainer/VBoxContainer/HeaderContainer/AddItemButton
-@onready var itemPopUpMenu: PopupMenu = $PanelContainer/VBoxContainer/HeaderContainer/ItemTypePopUpMenu
 @onready var inventory: VBoxContainer = $PanelContainer/VBoxContainer/InventoryScrollContainer/InventoryContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	addItemButton.button_up.connect(_on_add_item_button_press.bind())
-	itemPopUpMenu.index_pressed.connect(_on_item_menu_index_pressed.bind())
 	close_requested.connect(_on_close_requested)
-	
+
 func _input(event: InputEvent):
 	if event.is_action_pressed("ui_inventory"):
 		get_viewport().set_input_as_handled()
@@ -20,10 +18,10 @@ func _input(event: InputEvent):
 			hide_window()
 		else:
 			show_window()
-	
+
 func show_window():
 	visible = true
-	
+
 func hide_window():
 	visible = false
 
@@ -31,9 +29,6 @@ func _on_close_requested():
 	hide_window()
 
 func _on_add_item_button_press():
-	itemPopUpMenu.show()
+	var genericItemScene = load("res://components/InventoryComponent/ItemComponents/GenericItemComponent/GenericItemComponent.tscn")
+	inventory.add_child(genericItemScene.instantiate())
 
-func _on_item_menu_index_pressed(index: int):
-	if index == 0:
-		var weaponItemScene = load("res://components/InventoryComponent/ItemComponents/WeaponItemComponent/WeaponItemComponent.tscn")
-		inventory.add_child(weaponItemScene.instantiate())
